a96621cac8a6cece06a06254ee3a472cb30aa155
DanielSigala26 <L26550895@chihuahua2.tecnm.mx>
2026-09-11 18:18:41 -0600

    Add files via upload

---
+++ EVA1_4_Captura/src/main/java/com/mycompany/eva1_4_captura/EVA1_4_Captura.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_captura;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_4_Captura {
+
+    public static void main(String[] args) {
+        
+        String nombre;// aqui se declara la variable 
+        
+        System.out.println("Captura el nombre");
+        // entrada de datos
+        Scanner captu = new Scanner ((Readable) System.int);
+       
+    }
+}
