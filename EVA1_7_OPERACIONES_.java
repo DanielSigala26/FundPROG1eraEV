a55ad69f092919dd8d07942278885d1dad3cc7ba
DanielSigala26 <L26550895@chihuahua2.tecnm.mx>
2026-09-14 18:54:10 -0600

    Add files via upload

---
+++ EVA1_7_OPERACIONES_.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_operaciones;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_7_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //EL TIPO DE DATO DETERMINA LAS OPERACIONES
+        //ENTEROS, REALES, CADENAS, LOGICOS, ETC
+        //EXPRESION:
+        //VALORES
+        //VATRIABLES
+        //OPERADORES
+        //FUNCIONES
+        //QUE SE EVALUAN PARA OBTENER UN RESULTADO
+        //OPERACIONES CON NUMEROS:
+        //ARITMETICAS:
+        int suma, val1, val2; //declaro 3 variabbles del mismo tipo
+        //ASIGNACION: PONER UN VALOR EN UNA VARIABLE
+        //OPERADOR --> simbolo que trabaja SOBRE identificadores.
+        // = -->asignacion --> poner el valor
+        // == --> comparacion
+        val1 = 100; //asignar el valor de 100 a la variable val1
+        val2 = 50;
+        
+        suma val1 + val2; //expresion, asignar a la variable suma la entre val1 y val2 (+)
+        // + esta entre dos enteros
+        System.out.print("Vall = ");
+        System.out.println(val1);
+        System.out.println("Val2 = ");
+        System.out.println(val2);
+        System.out.println("suma");
+        System.out.println(suma);
+        
+       //MULTIPLICACION
+       int multi;
+       multi = val1* val2;
+       System.out.print("multiplicacion=");
+       System.out.println(multi);
+       
+       //Division
+       // (/)
+       
+        int divi;
+        divi = val1 / val2;
+        System.out.println("division");
+        System.out.println(divi);
+        val1 = 10;
+        val2 = 3;
+        divi = val1 / val2;
+        System.out.println("Val1=");
+        System.out.println(val1);
+        System.out.println("Val2=");
+        System.out.println(val2);
+        System.out.println("division");
+        System.out.println(divi);
+        
+    }
+    
+}
